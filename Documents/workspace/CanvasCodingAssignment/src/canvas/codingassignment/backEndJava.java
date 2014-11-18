525d60f0312bbb6f5fbf7051f1b8db0fb775038e
crimsonangel68 <valnir1@gmail.com>
2014-11-17 20:58:51 -0700

    Having issues with commandline commit

--- Documents/workspace/CanvasCodingAssignment/src/canvas/codingassignment/backEndJava.java
+++ Documents/workspace/CanvasCodingAssignment/src/canvas/codingassignment/backEndJava.java
@@ -1,20 +1,60 @@
 package canvas.codingassignment;
 
-import java.io.InputStream;
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-
 
 public class backEndJava 
 {
 	String authorizationToken = "9be624b4d5206a178fc56921d5bf2c2a";
-	URL courseURL;
-	try {
-		courseURL = new URL("http://canvas-api.herokuapp.com/api/v1/courses");}
-	catch(MalformedURLException e)
+	String courseAPIURL = "http://canvas-api.herokuapp.com/api/v1/courses";
+	
+	public backEndJava()
 	{
 		
 	}
 	
-	Object jsonData;
+	public static String getJSON()
+	{
+		
+		String result = "";
+		try {
+			URL courseURL = new URL("http://canvas-api.herokuapp.com/api/v1/courses?access_token=9be624b4d5206a178fc56921d5bf2c2a");
+			HttpURLConnection conn = (HttpURLConnection) courseURL.openConnection();
+//			if (conn.getResponseCode() != 200)
+//			{
+//				System.err.println("ERROR");
+//				throw new IOException(conn.getResponseMessage());
+//			}
+			
+			BufferedReader rd = new BufferedReader( new InputStreamReader(conn.getInputStream()));
+			StringBuilder sb = new StringBuilder();
+			String line;
+			while((line = rd.readLine()) != null)
+			{
+				sb.append(line);
+			}
+			rd.close();
+			conn.disconnect();
+			result = sb.toString();
+			
+			}
+		catch(MalformedURLException e)
+		{
+			System.err.println(e.toString());
+		}
+		catch(IOException e)
+		{
+			System.err.println(e.toString());
+		}
+		return result;
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		String temp = getJSON();
+		System.out.println(temp + "\n\n\n");
+	}
 }
